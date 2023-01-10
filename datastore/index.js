4d0b8eb120ce0401429f95de5bf2d0eeb8d2c6f5
Kevin Le <iamkevinvle@gmail.com>
2023-01-10 13:23:00 -0800

    solo complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -76,14 +76,20 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+
+  fs.readFile(path.join(exports.dataDir, `${id}.txt`), 'utf-8', (err, text) => {
+    if (err) {
+      callback(err)
+    } else {
+      fs.unlink(path.join(exports.dataDir, `${id}.txt`), (err) => {
+        if (err) {
+          callback(err)
+        } else {
+          callback()
+        }
+      })
+    }
+  })
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
